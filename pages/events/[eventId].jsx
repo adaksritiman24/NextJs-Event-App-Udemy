3a4ca06e8e2c2ffe0c774de713109d07eacdb003
Sritiman Adak <adaksritiman24@gmail.com>
2022-08-21 10:01:51 +0530

    EventDetails page added

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/eventDetails/EventSummary";
+import EventLogistics from "../../components/eventDetails/EventLogistics";
+import EventContent from "../../components/eventDetails/EventContent";
 
 export default function EventDetailsPage() {
-    const router = useRouter();
+  const router = useRouter();
+
+  const event = getEventById(router.query.eventId);
+
+  if(!event) return <p>No Event Found!</p>
   return (
-    <h1>Event Details: {router.query.eventId}</h1>
-  )
+    <>
+      <EventSummary title={event.title}/>
+      <EventLogistics {...{...event, address : event.location}} imageAlt={event.title}/>
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
